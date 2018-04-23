231736b5a288364e2016f30123e85c72aabfa1bf
vickyhovey <vickyhovey@gmail.com>
2018-04-22 22:57:14 -0400

    models

--- perfectcushion/shop/models.py
+++ perfectcushion/shop/models.py
@@ -7,5 +7,28 @@ class Category(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='category', blank=True)
 
-class Meta:
-    ordering = ('name', )
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    def __str__(self):
+        return '{}'.format(self.name)
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product', blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+    def __str__(self):
+        return '{}'.format(self.name)
